195929ccd438ac3843289f1ca5c5b5e8b61579df
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-14 18:52:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 const { validateStep1, validateStep2 } = require('./validation');
-
+const path = require('path');
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const path = require('path');
 
+ app.use(cors(
+));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
@@ -17,9 +18,7 @@ app.get('*', (req, res) => {
 });
 
 
- app.use(cors({
-  origin: "https://udyamr.onrender.com"
-}));
+
 
         
 
