63b4ad4bd49ae453875cbec4c8436f4ffdf434d0
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-14 18:58:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
- app.use(cors(
-));
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
